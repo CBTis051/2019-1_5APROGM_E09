a182322b4ea7bd8d18fd870d72f02105bf1a6e24
Jeús Omar Pérez Sánchez <al17235@cbtis051.edu.mx>
2019-11-25 11:19:39 -0700

    Ajustes en la respuesta a formato JSON

--- app_al17235.js
+++ app_al17235.js
@@ -1,6 +1,14 @@
 var http = require('http');
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-    res.end('Hola  Mundo Node js Repo GIT - Pérez Sánchez Jesús Omar/n');
+    
+    var objeto = {
+        mensaje : 'Hola mundo Node JS Repo GIT - Pérez Sánchez Jesús Omar \n'
+    };
+
+    var json = JSON.stringify(objeto);
+
+    res.end(json);
+
 }).listen(8081);
 console.log('Server running on port 8081');
